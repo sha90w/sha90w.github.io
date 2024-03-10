532c21de3d29af31b2c6339276c2fbf509a7d66f
Michail Litvak <sha90w@gmail.com>
2024-03-10 11:59:17 +0100

    .

--- k8s-aliases.sh
+++ k8s-aliases.sh
@@ -1,4 +1,4 @@
 alias k=kubectl
 alias kgp="kubectl get pods"
 function klsc() { kubectl get pod "$1" -o json | jq '.spec.containers[].name'; }
-function kexb() { kubectl exec -it "$1" -c "$2" -- bash }
+function kexb() { kubectl exec -it "$1" -c "$2" -- bash i ; }
