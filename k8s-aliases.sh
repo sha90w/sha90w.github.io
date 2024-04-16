fb50b637763b1f02657b64ac50eed3159394f7cd
Michail Litvak <sha90w@gmail.com>
2024-04-16 18:44:24 +0200

    Add alias for kubectl logs

--- k8s-aliases.sh
+++ k8s-aliases.sh
@@ -2,3 +2,4 @@ alias k=kubectl
 alias kgp="kubectl get pods"
 function klsc() { kubectl get pod "$1" -o json | jq '.spec.containers[].name'; }
 function kexb() { kubectl exec -it "$1" -c "$2" -- bash ; }
+function klog() { kubectl logs "$1" -c "$2" --timestamps --since="$3" ; }
